a4f2eda0f893d2d625f731f42d3a588403f79444
David <zaharandavid1@gmail.com>
2024-09-07 21:35:01 +0300

    Realized all metods

--- Task2.cs
+++ Task2.cs
@@ -14,13 +14,25 @@
         Console.Write("\nEnter the Real part of complex number: ");
         this.RealPart = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("\nEnter the Imaginary part of complex number: ");
+        Console.Write("Enter the Imaginary part of complex number: ");
         this.ImaginaryPart = Convert.ToDouble(Console.ReadLine());
     }
 
     public void PrintComplexNumber()
     {
         Console.WriteLine($"\nReal part:{this.RealPart} Imaginary part:{this.ImaginaryPart}");
+    }
+
+    public void ModuleComplexNumber()
+    {
+        double module = Math.Sqrt(Math.Pow(this.RealPart, 2) + Math.Pow(this.ImaginaryPart, 2));
+        Console.WriteLine($"\nModule of complex number:{module}");
+    }
+
+    public void ArgComplexNumber()
+    {
+        double arg = this.RealPart >= 0 ? (Math.Atan(this.ImaginaryPart / this.RealPart)) : (Math.Atan(this.ImaginaryPart / this.RealPart) + Math.PI);
+        Console.WriteLine($"\nArgiment of complex number:{arg}");
     }
 }
 
@@ -51,8 +63,26 @@ partial class Program
 
         double resultReal, resultImaginary;
 
-        resultReal = (complexNumber.RealPart * newNumber.RealPart + -1 * complexNumber.ImaginaryPart * newNumber.ImaginaryPart);
-        resultImaginary = (complexNumber.ImaginaryPart * newNumber.RealPart + complexNumber.RealPart * newNumber.ImaginaryPart));
+        resultReal = (complexNumber.RealPart * newNumber.RealPart - complexNumber.ImaginaryPart * newNumber.ImaginaryPart);
+        resultImaginary = (complexNumber.ImaginaryPart * newNumber.RealPart + complexNumber.RealPart * newNumber.ImaginaryPart);
+
+        complexNumber.RealPart = resultReal;
+        complexNumber.ImaginaryPart = resultImaginary;
+    }
+
+    public static void DivComplexNumbers(ref ComplexNumber complexNumber)
+    {
+        ComplexNumber newNumber = new ComplexNumber();
+        newNumber.ChangeComlexNumber();
+
+        double resultReal, resultImaginary;
+
+        resultReal = (complexNumber.RealPart * newNumber.RealPart + complexNumber.ImaginaryPart * newNumber.ImaginaryPart) / (Math.Pow(newNumber.RealPart, 2) + Math.Pow(newNumber.ImaginaryPart, 2));
+
+        resultImaginary = (complexNumber.ImaginaryPart*newNumber.RealPart - complexNumber.RealPart * newNumber.ImaginaryPart) / (Math.Pow(newNumber.RealPart, 2) + Math.Pow(newNumber.ImaginaryPart, 2));
+
+        complexNumber.RealPart = resultReal;
+        complexNumber.ImaginaryPart = resultImaginary;
     }
 
     public static void Main(string[] args)
@@ -72,23 +102,27 @@ partial class Program
                     break;
 
                 case 'b':
-                    Program.SumComplexNumbers(ref complexNumber);
+                    SumComplexNumbers(ref complexNumber);
                     break;
 
                 case 'c':
-                    Program.DifComplexNumbers(ref complexNumber);
+                    DifComplexNumbers(ref complexNumber);
                     break;
 
                 case 'd':
+                    MultComplexNumbers(ref complexNumber);
                     break;
 
                 case 'e':
+                    DivComplexNumbers(ref complexNumber);
                     break;
 
                 case 'f':
+                    complexNumber.ModuleComplexNumber();
                     break;
 
                 case 'g':
+                    complexNumber.ArgComplexNumber();
                     break;
 
                 case 'h':
