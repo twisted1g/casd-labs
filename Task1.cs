17522afb6aafb274239063dfdf9638205e81141c
David <zaharandavid1@gmail.com>
2024-09-07 20:19:01 +0300

    Reworked structure and update try-catch block

--- Task1.cs
+++ Task1.cs
@@ -1,77 +1,81 @@
-﻿void MatrixPrint(int[,] matrix, int dim)
+﻿partial class Program
 {
-    for (int i = 0; i < dim; i++)
+   static void MatrixPrint(int[,] matrix, int dim)
     {
-        for (int j = 0; j < dim; j++) Console.Write($"{matrix[i, j]} ");
+        for (int i = 0; i < dim; i++)
+        {
+            for (int j = 0; j < dim; j++) Console.Write($"{matrix[i, j]} ");
+            Console.WriteLine();
+        }
         Console.WriteLine();
     }
-    Console.WriteLine();
-}
 
-void PrintVector(int[] vector, int dim)
-{
-    for (int j = 0; j < dim; j++) Console.Write($"{vector[j]} ");
-    Console.WriteLine();
-}
-
-bool IsSymmetric(int[,] matrix, int dim)
-{
-    for (int i = 0; i < dim; i++)
-        for (int j = 0; j < dim; j++) if (matrix[i, j] != matrix[j, i]) return false;
-    return true;
-}
-
-double MatrixMult(int[,] matrix, int[] vector, int dim)
-{
-    int[] new_vector1 = new int[dim];
-    for (int i = 0; i < dim; i++)
+    static void PrintVector(int[] vector, int dim)
     {
-        int sum = 0;
-        for (int j = 0; j < dim; j++)
+        for (int j = 0; j < dim; j++) Console.Write($"{vector[j]} ");
+        Console.WriteLine();
+    }
+
+    static bool IsSymmetric(int[,] matrix, int dim)
+    {
+        for (int i = 0; i < dim; i++)
+            for (int j = 0; j < dim; j++) if (matrix[i, j] != matrix[j, i]) return false;
+        return true;
+    }
+
+    static double MatrixMult(int[,] matrix, int[] vector, int dim)
+    {
+        int[] new_vector1 = new int[dim];
+        for (int i = 0; i < dim; i++)
         {
-            sum += vector[j] * matrix[i, j];
+            int sum = 0;
+            for (int j = 0; j < dim; j++)
+            {
+                sum += vector[j] * matrix[i, j];
+            }
+            new_vector1[i] = sum;
         }
-        new_vector1[i] = sum;
+        double result = 0;
+        for (int i = 0; i < dim; i++) result += new_vector1[i] * new_vector1[i];
+
+        result = Math.Sqrt(result);
+        return result;
     }
-    double result = 0;
-    for (int i = 0; i < dim; i++) result += new_vector1[i] * new_vector1[i];
-
-    result = Math.Sqrt(result);
-    return result;
-}
-
-try
-{
-    StreamReader sr = new StreamReader(@"D:\file.txt.txt");
-    int dim = Convert.ToInt32(sr.ReadLine());
-    Console.WriteLine(dim);
-
-
-    int[,] matrix = new int[dim, dim];
-    for (int i = 0; i < dim; i++)
+    public static void Main(string[] args)
     {
-        string[] nums = sr.ReadLine().Split(" ");
-        for (int j = 0; j < dim; j++) matrix[i, j] = Convert.ToInt32(nums[j]);
+        try
+        {
+            StreamReader sr = new StreamReader(@"D:\file.txt.txt");
+            int dim = Convert.ToInt32(sr.ReadLine());
+            //Console.WriteLine(dim);
+
+            int[,] matrix = new int[dim, dim];
+            for (int i = 0; i < dim; i++)
+            {
+                string[] nums = sr.ReadLine().Split(" ");
+                for (int j = 0; j < dim; j++) matrix[i, j] = Convert.ToInt32(nums[j]);
+            }
+            //MatrixPrint(matrix, dim);
+
+            string line = sr.ReadLine();
+            int[] vector = line.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            //PrintVector(vector, dim);
+
+            sr.Close();
+
+            if (IsSymmetric(matrix, dim))
+            {
+                Console.WriteLine("Matrix is symmetric");
+
+                double VectorLenght = MatrixMult(matrix, vector, dim);
+                Console.WriteLine($"Lenght of vector in {dim}-space is {VectorLenght}");
+            }
+            else throw new Exception("Matrix is not symmetric");
+        }
+        catch(Exception message)
+        {
+            Console.WriteLine("Error" + message.Message);
+        }
     }
-    MatrixPrint(matrix, dim);
-
-    string line = sr.ReadLine();
-    int[] vector = line.Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
-
-    PrintVector(vector, dim);
-
-    sr.Close();
-
-    if (IsSymmetric(matrix, dim))
-    {
-        Console.WriteLine("True");
-
-        double result = MatrixMult(matrix, vector, dim);
-        Console.WriteLine(result);
-    }
-    else throw new Exception();
-}
-catch
-{
-    Console.WriteLine("Error");
 }
