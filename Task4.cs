f2a3bd5a9795f6e568e6d58ca0731a6388fa1464
David <zaharandavid1@gmail.com>
2024-09-19 20:33:37 +0300

    Add few methods

---
+++ Task4.cs
@@ -0,0 +1,48 @@
+﻿class MyArrayList<T>
+{
+    T[] elementData;
+    int size;
+
+    public MyArrayList()
+    {
+        elementData = new T[0];
+        size = 0;
+    }
+
+    public MyArrayList(T[] array)
+    {
+        this.elementData = array;
+    }
+
+    public MyArrayList(int capacity)
+    {
+        elementData = new T[capacity];
+        size = capacity;
+    }
+
+    public void PrintMyArrayList()
+    {
+        foreach (T item in elementData)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+}
+
+
+
+partial class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = { 1, 2 };
+        MyArrayList<int> myArrayList = new MyArrayList<int>(arr);
+
+        myArrayList.PrintMyArrayList();
+        arr = new int[]{1, 2, 3, 4 };
+
+        myArrayList.PrintMyArrayList();
+
+    }
+}
