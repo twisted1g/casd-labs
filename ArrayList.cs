7d8672db5287cf78cbe50b55170ed84e93e15f77
David <zaharandavid1@gmail.com>
2024-09-23 20:40:05 +0300

    Made all features

---
+++ ArrayList.cs
@@ -0,0 +1,242 @@
+﻿namespace ArrayList
+{
+    class MyArrayList<T>
+    {
+        T[] elementData;
+        private int size;
+
+        public MyArrayList()
+        {
+            elementData = new T[0];
+            size = 0;
+        }
+
+        public MyArrayList(T[] array)
+        {
+            elementData = new T[0];
+            AddAll(array);
+        }
+
+        public MyArrayList(int capacity)
+        {
+            elementData = new T[capacity];
+            size = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (size >= elementData.Length)
+            {
+                T[] copyElementData = new T[(int)(elementData.Length * 1.5) + 1];
+                Array.Copy(elementData, copyElementData, size);
+                elementData = copyElementData;
+            }
+            elementData[size++] = item;
+        }
+
+        public void AddAll(T[] array)
+        {
+            foreach (T item in array)
+            {
+                Add(item);
+            }
+        }
+
+        public void Clear()
+        {
+            elementData = new T[0];
+            size = 0;
+        }
+
+        public bool Contains(object obj)
+        {
+           
+            foreach (T item in elementData)
+            {
+                if (obj.Equals(item)) return true;
+            }
+            return false;
+        }
+
+        public bool ContainsAll(T[] array)
+        {
+            foreach (T item in array)
+            {
+                if (!Contains(item)) return false;
+            }
+            return true;
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        public void Remove(object obj)
+        {
+            if (Contains(obj))
+            {
+                T[] copyElementData = new T[--size];
+                int ind = 0;
+                foreach (T item in elementData)
+                {
+                    if (obj.Equals(item)) continue;
+                    else copyElementData[ind++] = item;
+
+                }
+                elementData = copyElementData;
+            }
+        }
+
+        public void RemoveAll(T[] array)
+        {
+            if (Contains(array))
+            {
+                foreach (T item in array) Remove(item);
+            }
+        }
+
+        public void RetainAll(T[] array)
+        {
+            if (ContainsAll(array))
+            {
+                foreach (T item in elementData)
+                {
+                    bool flag = false;
+                    foreach (T newItem in array)
+                    {
+                        if (item.Equals(newItem))
+                        {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) Remove(item);
+                }
+                size = array.Length;
+            }
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = elementData[i];
+            }
+            return array;
+        }
+
+        public T[] ToArray(T[] array)
+        {
+            if (array == null) array = new T[size];
+            //for (int i = 0; i  < size; i++) array[i] = elementData[i];
+            array = ToArray();
+            return array;
+        }
+
+        public void Add(int index, T item)
+        {
+            if (index >= 0 && index < size)
+            {
+                T[] copyElementData = new T[++size];
+                bool flag = false;
+                for (int i = 0; i < size; i++)
+                {
+                    if (i == index)
+                    {
+                        flag = true;
+                        copyElementData[i] = item;
+                    }
+                    else if (flag) copyElementData[i] = elementData[i - 1];
+                    else copyElementData[i] = elementData[i];
+                }
+                elementData = copyElementData;
+            }
+        }
+
+        public void Add(int index, T[] array)
+        {
+            foreach (T item in array)
+            {
+                Add(index++, item);
+            }
+        }
+
+        public T Get(int index)
+        {
+            if ((index >= 0) && (index < size)) return elementData[index];
+            else throw new IndexOutOfRangeException();
+        }
+
+        public int IndexOf(object obj)
+        {
+            if (Contains(obj))
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (obj.Equals(elementData[i])) return i;
+                }
+            }
+            return -1;
+        }
+
+        public int LastIndexOf(object obj)
+        {
+            if (Contains(obj))
+            {
+                int lastIndex = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    if (obj.Equals(elementData[i])) lastIndex = i;
+                }
+                return lastIndex;
+            }
+            return -1;
+        }
+
+        public T Remove(int index)
+        {
+            T item = Get(index);
+            Remove(item);
+            return item;
+        }
+
+        public void Set(int index, T item)
+        {
+            if (index >= 0 && index < size)
+            {
+                elementData[index] = item;
+            }
+        }
+
+        public T[] SubList(int fromIndex, int toIndex)
+        {
+            if (fromIndex >= 0 && fromIndex < toIndex && toIndex < size)
+            {
+                T[] subList = new T[toIndex - fromIndex];
+                for (int i = fromIndex; i < toIndex; i++)
+                {
+                    subList[i - fromIndex] = elementData[i];
+                }
+                return subList;
+            }
+            else throw new IndexOutOfRangeException();
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(elementData[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using ArrayList;
+
+class Program
+{   
+    public static bool IsTag(string tag)
+    {
+        if (string.IsNullOrEmpty(tag)) return false;
+        else
+        {
+            if (tag.Length < 3) return false;
+            int cntOpen = 0;
+            int cntClose = 0;
+            int cntSlash = 0;
+            if (tag[0] != '<' ) return false;
+            
+            if (!(tag[1] == '/' && char.IsLetter(tag[2]) || char.IsLetter(tag[1]))) return false;
+
+            if (tag[^1] != '>') return false;
+
+            for (int i = 0; i < tag.Length; i++)
+            {
+                if (cntOpen == 0 && tag[i] == '<') cntOpen++;
+                else if (cntOpen == 1 && tag[i] == '<') return false;
+
+                if (cntClose == 0 && tag[i] == '>') cntClose++;
+                else if (cntClose == 1 && tag[i] == '>') return false;
+
+                if (cntSlash == 0 && tag[i] == '/') cntSlash++;
+                else if (cntSlash == 1 && tag[i] == '/') return false;
+
+                if (!(tag[i] == '<' || tag[i] == '>' || tag[i] == '/' || char.IsDigit(tag[i]) || char.IsLetter(tag[i])))
+                    return false;
+                
+            }
+            return true;
+        }
+    }
+
+    public static bool CompareTags(string firstTag,  string secondTag)
+    {
+        if (firstTag is not null && secondTag is not null)
+        {
+            string innerFirstTag = "";
+            for (int i = 0; i < firstTag.Length; i++)
+            {
+                if (firstTag[i] == '<' || firstTag[i] == '>' || firstTag[i] == '/') continue;
+                else innerFirstTag += firstTag[i];
+            }
+
+            string innerSecondTag = "";
+            for (int i = 0; i < secondTag.Length; i++)
+            {
+                if (secondTag[i] == '<' || secondTag[i] == '>' || secondTag[i] == '/') continue;
+                else innerSecondTag += secondTag[i];
+            }
+
+            return innerFirstTag.ToUpper() == innerSecondTag.ToUpper();
+        }
+        return false;
+    }  
+
+    public static void ReadFile(ref MyArrayList<string> myArrayList)
+    {
+        using (StreamReader reader = new StreamReader("Text.txt"))
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                string tag = "";
+                int cnt = 0;
+                for (int i = 0; i < line.Length; i++)
+                {   
+                    if (cnt <= 1 && line[i] == '<')
+                    {
+                        tag = "";
+                        tag += line[i];
+                        cnt = 1;
+                    }
+
+                    if (cnt == 1 && line[i] != '<') tag += line[i];
+                   
+                    if (line[i] == '>')
+                    {
+                        if (IsTag(tag)) myArrayList.Add(tag);
+
+                        tag = "";
+                        cnt = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void DeleteTimes(ref MyArrayList<string> myArrayList)
+    {
+        int countUnique = 0;
+
+        // Ищем количество уникальных элементов
+        for (int i = 0; i < myArrayList.Size(); i++)
+        {
+            bool isUnique = true;
+            for (int j = 0; j < countUnique; j++)
+            {
+                if (CompareTags(myArrayList.Get(i), myArrayList.Get(j)))
+                {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) myArrayList.Set(countUnique++, myArrayList.Get(i));
+            
+        }
+
+        MyArrayList<string> copyMyArrayList = new MyArrayList<string>(myArrayList.SubList(0, countUnique));
+        myArrayList = copyMyArrayList;
+    }
+
+    public static void Main(string[] args)
+    {
+        MyArrayList<string> myArrayList = new MyArrayList<string>();
+
+        ReadFile(ref myArrayList);
+        myArrayList.Print();
+        
+        DeleteTimes(ref myArrayList);
+        myArrayList.Print();
+    }
+}
