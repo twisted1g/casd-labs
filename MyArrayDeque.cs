06c038fe679bb5842ab9d553e1c1d0dde2651c67
David <zaharandavid1@gmail.com>
2024-11-10 22:24:28 +0300

    Add all main features

---
+++ MyArrayDeque.cs
@@ -0,0 +1,328 @@
+﻿namespace ArrayDeque;
+
+class MyArrayDeque<T>
+{
+    T[] elements;
+
+    public int Head { get; private set; } = 0;
+
+    public int Tail { get; private set; } = -1;
+
+    public MyArrayDeque() : this(16) { }
+
+    public MyArrayDeque(T[] array)
+    {
+        elements = new T[array.Length];
+        Array.Copy(array, elements, array.Length);
+        Tail = array.Length - 1;
+    }
+
+    public MyArrayDeque(int numElements)
+    {
+        elements = new T[numElements];
+    }
+
+    public void Add(T item)
+    {
+        if (Tail >= elements.Length - 1)
+        {
+            T[] copyElements = new T[elements.Length * 2 + 1];
+            Array.Copy(elements, copyElements, elements.Length);
+            elements = copyElements;
+        }
+        elements[++Tail] = item;
+    }
+
+    public void AddAll(T[] array)
+    {
+        foreach(T item in array)
+        {
+            Add(item);
+        }
+    }
+
+    public void Clear()
+    {
+        elements = new T[1];
+        Tail = -1;
+    }
+
+    public bool Contains(T obj)
+    {
+        return Array.LastIndexOf(elements, obj, Tail) >= 0;
+    }
+
+    public bool ContainsAll(T[] array)
+    {
+        foreach (T item in array)
+        {
+            if (!Contains(item)) return false;
+        }
+        return true;
+    }
+
+    public bool IsEmpty()
+    {
+        return Tail == -1;
+    }
+
+    public void RemoveAt(int deleteIndex)
+    {
+        if (deleteIndex <= Tail && deleteIndex >= Head)
+        {
+            T[] copyElements = new T[elements.Length];
+            int ind = 0;
+            for (int i = 0; i <= Tail; i++)
+            {
+                if (i == deleteIndex) continue;
+                else copyElements[ind++] = elements[i];
+            }
+            elements = copyElements;
+            Tail--;
+        }
+        else throw new IndexOutOfRangeException();
+
+    }
+
+    public void Remove(T obj)
+    {
+        if (Contains(obj))
+        {
+            int index = Array.LastIndexOf(elements, obj, Tail);
+            RemoveAt(index);
+        }
+    }
+
+    public void RemoveAll(T[] array)
+    {
+        if (ContainsAll(array))
+        {
+            foreach (T item in array)
+            {
+                Remove(item);
+            }
+        }
+    }
+
+    public void RetainAll(T[] array)
+    {
+        if (ContainsAll(array))
+        {
+            T[] copyElementData = new T[array.Length];
+            int ind = 0;
+            foreach (T item in array)
+            {
+                copyElementData[ind++] = elements[Array.LastIndexOf(elements, item, Tail)];
+            }
+            elements = copyElementData;
+            Tail = copyElementData.Length - 1;
+        }
+    }
+
+    public int Size()
+    {
+        return Tail + 1;
+    }
+
+    public T[] ToArray()
+    {
+        T[] newElements = new T[Tail + 1];
+        Array.Copy(elements, newElements, Tail + 1);
+        return newElements;
+    }
+
+    public T[] ToArray(T[] array)
+    {
+        if (array == null) array = new T[Tail + 1];
+        array = ToArray();
+        return array;
+    }
+
+    public T? Element()
+    {
+        if (Tail == -1)
+        {
+            return default(T);
+        }
+        return elements[Head];
+    }
+
+    public bool Offer(T item)
+    {
+        Add(item);
+        return true;
+    }
+
+    public T? Peek()
+    {
+        if (Tail == -1)
+        {
+            return default(T);
+        }
+        return elements[Head];
+    }
+
+    public T? Poll()
+    {
+        if (Tail == -1)
+        {
+            return default(T);
+        }
+        T res = elements[Head];
+        RemoveAt(Head);
+
+        return res;
+    }
+
+    public void AddFirst(T item)
+    {
+        T[] copyElements = new T[elements.Length];
+        if (Tail >= elements.Length - 1)
+        {
+            copyElements = new T[elements.Length * 2 + 1]; 
+            
+        }
+        Array.Copy(elements, copyElements, elements.Length);
+
+        for (int i = Tail + 1; i > Head; i--)
+        {
+            copyElements[i] = elements[i - 1];
+        }
+        copyElements[Head] = item;
+        elements = copyElements;
+        Tail++;
+    }
+
+    public void AddLast(T item)
+    {
+        Add(item);
+    }
+
+    public T? GetFirst()
+    {
+        if (Tail != -1)
+        {
+            return elements[Head];
+        }
+        else throw new IndexOutOfRangeException();
+    }
+
+    public T? GetLast()
+    {
+        if (Tail != -1)
+        {
+            return elements[Tail];
+        }
+        else throw new IndexOutOfRangeException();
+    }
+
+    public bool OfferFirst(T item)
+    {
+        if (Tail < elements.Length)
+        {
+            AddFirst(item);
+            return true;
+        }
+        return false;
+    }
+
+    public bool OfferLast(T item)
+    {
+        if (Tail < elements.Length)
+        {
+            AddLast(item);
+            return true;
+        }
+        return false;
+    }
+
+    public T Pop()
+    {
+        if (Tail == -1)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        T res = elements[Head];
+        RemoveAt(Head);
+
+        return res;
+    }
+
+    public void Push(T item)
+    {
+        AddFirst(item);
+    }
+
+    public T? PeekFirst()
+    {
+        return Peek();
+    }
+
+    public T? PeekLast()
+    {
+        if (Tail == -1)
+        {
+            return default(T);
+        }
+        return elements[Tail];
+    }
+
+    public T? PollFirst()
+    {
+        return Poll();
+    }
+
+    public T? PollLast()
+    {
+        if (Tail == -1)
+        {
+            return default(T);
+        }
+        T res = elements[Tail];
+        RemoveAt(Tail);
+
+        return res;
+    }
+
+    public T RemoveFirst()
+    {
+        if (Tail != -1)
+        {
+            T res = elements[Head];
+            RemoveAt(Head);
+            return res;
+        }
+        else throw new IndexOutOfRangeException();
+    }
+
+    public T RemoveLast()
+    {
+        if (Tail != -1)
+        {
+            T res = elements[Tail];
+            RemoveAt(Tail);
+            return res;
+        }
+        else throw new IndexOutOfRangeException();
+    }
+
+    public void RemoveFirstOccurrence(T item)
+    {
+        RemoveAt(Array.IndexOf(elements, item));
+    }
+
+    public void RemoveLastOccurrence(T item)
+    {
+        Remove(item);
+    }
+
+    public override string? ToString()
+    {
+        if (elements is not null)
+            return string.Join(" ", elements);
+
+        return null;
+    }
+
+
+}
---
+++ Task15.cs
@@ -0,0 +1,92 @@
+﻿using ArrayDeque;
+
+class Program
+{
+    static int CountNumbersInString(string? currentString)
+    {
+        int countNumbers = 0;
+        if (currentString is not  null )
+        {
+            for (int i = 0; i < currentString.Length; i++)
+            {
+                if (Char.IsDigit(currentString[i])) countNumbers++;
+            }
+        }
+        return countNumbers;
+    }
+
+    static int CountSpacesInString(string? currentString)
+    {
+        int countSpaces = 0;
+        if (currentString is not null)
+        {
+            for (int i = 0; i < currentString.Length; i++)
+            {
+                if (currentString[i] == ' ') countSpaces++;
+            }
+        }
+        return countSpaces;
+    }
+
+    static bool CompareNumbersInString(string? firstString, string? secondString)
+    {
+        if (CountNumbersInString(firstString) >= CountNumbersInString(secondString)) return true;
+        else return false;
+    }
+
+    static void ReadFromFile(ref MyArrayDeque<string> deque)
+    {
+        using (StreamReader reader = new StreamReader("input.txt"))
+        {
+            string? currentString;
+            while ((currentString = reader.ReadLine()) != null)
+            {
+                if (CompareNumbersInString(currentString, deque.PeekFirst()))
+                {
+                    deque.AddLast(currentString);
+                }
+                else deque.AddFirst(currentString);
+            }
+        }
+    }
+
+    static void WriteToFile(MyArrayDeque<string> deque)
+    {
+        using (StreamWriter wtiter = new StreamWriter("sorted.txt"))
+        {
+            while(deque.Size() != 0)
+            {
+                wtiter.WriteLine(deque.PollFirst());
+            }
+        }
+    }
+
+    static MyArrayDeque<string> DeleteWrongLines(MyArrayDeque<string> deque, int countSpaces)
+    {
+        MyArrayDeque<string> newDeque = new MyArrayDeque<string>();
+        while(deque.Size() != 0)
+        {
+            string? currentString = deque.PollFirst();
+            if (CountSpacesInString(currentString) < countSpaces)
+            {
+                newDeque.AddLast(currentString);
+            }
+        }
+        return newDeque;
+    }
+
+    static void Main(string[] args)
+    {
+        MyArrayDeque<string> deque = new MyArrayDeque<string>();    
+        ReadFromFile(ref deque);
+        Console.WriteLine(deque.ToString());
+        MyArrayDeque<string> copyDeque = new MyArrayDeque<string>(deque.ToArray());
+        WriteToFile(copyDeque);
+
+        int numbersOfSpaces = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(deque.ToString());
+        copyDeque = new MyArrayDeque<string>(deque.ToArray());
+        deque = DeleteWrongLines(copyDeque, numbersOfSpaces);
+        Console.WriteLine(deque.ToString());
+    }
+}
