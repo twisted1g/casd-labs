fcefa7ca8cc0c2e6385fb2390dae4944adee3576
David <zaharandavid1@gmail.com>
2024-11-30 23:47:38 +0300

    Add all features

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        //MyBinarySearchTree<int, int?> myBinarySearchTree = new MyBinarySearchTree<int, int?>();
+        //myBinarySearchTree.Add(1, 11);
+        //myBinarySearchTree.Add(2, 12);
+        //myBinarySearchTree.Add(8, 12);
+        //myBinarySearchTree.Add(5, 12);
+        //myBinarySearchTree.Add(3, 12);
+        //Console.WriteLine(myBinarySearchTree.Contains(3));
+
+        //Console.WriteLine(myBinarySearchTree);
+
+        //myBinarySearchTree.Remove(3);
+        //myBinarySearchTree.Add(8, 17);
+        //myBinarySearchTree.Remove(1);
+        //Console.WriteLine(myBinarySearchTree);
+
+        MyTreeMap<int, int> treeMap = new MyTreeMap<int, int>();
+        
+
+    }
+}
